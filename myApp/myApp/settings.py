c21399288879de5c39abe4e80bfac80f545adb38
srinivasmakkena <makkenaece@gmail.com>
2024-08-15 04:52:45 -0500

    .

--- myApp/myApp/settings.py
+++ myApp/myApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xcx+iyc8&wtv^gx_c5rt7tn3$gmn8+qwu(c7e-xdgv2&kmlk0&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
