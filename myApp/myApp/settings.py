66bfd2d136291c25240b4e3038d1853a2aa50a3f
srinivasmakkena <makkenaece@gmail.com>
2024-11-13 04:20:40 -0600

    .

--- myApp/myApp/settings.py
+++ myApp/myApp/settings.py
@@ -117,12 +117,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-STATICFILES_DIRS = [ # Here
-    BASE_DIR / 'core/static/'
-]
 
-STATIC_ROOT = BASE_DIR / 'static/'
-STATIC_URL = 'static/'# Default primary key field type
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'# Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
