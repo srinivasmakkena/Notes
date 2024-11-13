fb6600b4961e17e0d850a103db2938343f5f8f2f
Srinivas Makkena <32659482+srinivasmakkena@users.noreply.github.com>
2024-11-13 04:43:35 -0600

    Delete myApp copy directory

--- myApp copy/project/admin.py
+++
@@ -1,9 +0,0 @@
-# admin.py
-
-from django.contrib import admin
-from .models import Page
-
-@admin.register(Page)
-class PageAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    search_fields = ('name',)
